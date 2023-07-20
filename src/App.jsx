1c051a89e16364f55c0aaccdc841b15996566c9d
oc34n <ocean.wrng@gmail.com>
2023-07-19 23:17:21 -0400

    Implementing Navigation component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.scss';
+import Navigation from './components/Navigation';
 import ShoppingPage from './components/ShoppingPage';
 
 function App() {
   return (
     <main>
+      <Navigation/>
       <ShoppingPage />
     </main>
   )
