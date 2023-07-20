691ad1722dce9033d1285e339ff795c3d7c3d08a
oc34n <ocean.wrng@gmail.com>
2023-07-19 22:03:22 -0400

    Selected product now displays ProductBanner
    >From Product banner user may select product size and quantity to add to cart

--- src/components/ProductBanner.jsx
+++ src/components/ProductBanner.jsx
@@ -1,8 +1,11 @@
 import React, {useState} from 'react';
+import { useAtom } from 'jotai';
+import { displayProdAtom } from './ProductCard';
 import '../css/Product.scss';
 
-export default function ProductBanner() {
+export default function ProductBanner({productCompany, productName, productRating, productPrice, productId}) {
     const [numOfProduct, setProductNum] = useState(0);
+    const [closeDisplay, setCloseDisplay] = useAtom(displayProdAtom)
 
     function IncrCount(prevCount){
         return prevCount + 1
@@ -16,30 +19,49 @@ export default function ProductBanner() {
     }
 
   return (
-    <section className='productView'>
-        <section className='productImages'>
-            <img src='#'/>
-            <div className='imageCarousel'>
-            <p>Mapped Images</p>
-            </div>
+    <section className="productView">
+      <button
+        className="closeBtn"
+        onClick={() => setCloseDisplay(!closeDisplay)}>X</button>
+      <section className="productImages">
+        <img src="#" />
+        <div className="imageCarousel">
+          <p>Mapped Images</p>
+        </div>
+      </section>
+      <section className="productDetails">
+        <div>
+          <p>{productCompany}</p>
+          <h1>{productName}</h1>
+          <aside>{productRating}</aside>
+          <aside>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras varius
+            sodales justo, in varius ipsum aliquet quis. Nam lorem nulla,
+            aliquam id tellus sit amet, tristique fermentum sapien. 
+          </aside>
+          <h2>${productPrice}</h2>
+          <label htmlFor="product-size">Size: </label>
+          <select id="product-size" name="product-size">
+            <option value="xs">XS</option>
+            <option value="s">S</option>
+            <option value="m">M</option>
+            <option value="l">L</option>
+            <option value="xl">XL</option>
+          </select>
+        </div>
+        <section className="checkout">
+          <div>
+            <button onClick={() => setProductNum((prev) => DecrCount(prev))}>
+              -
+            </button>
+            <p>{numOfProduct}</p>
+            <button onClick={() => setProductNum((prev) => IncrCount(prev))}>
+              +
+            </button>
+          </div>
+          <button>Add to cart</button>
         </section>
-        <section className='productDetails'>
-            <div>
-                <p>Product Company</p>
-                <h1>PRODUCT</h1>   
-                <aside>product details..</aside>
-                <h2>$0</h2>
-                <aside>$oldprice</aside>
-            </div>
-            <section className='checkout'>
-                <div>
-                    <button onClick={() => setProductNum(prev => DecrCount(prev))}>-</button>
-                    <p>{numOfProduct}</p>
-                    <button onClick={() => setProductNum(prev => IncrCount(prev))}>+</button>
-                </div>
-                <button>Add to cart</button>
-            </section>
-        </section>
+      </section>
     </section>
-  )
+  );
 }
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,21 +1,39 @@
 import React, { useState } from 'react';
+import { atom, useAtom } from 'jotai';
 import '../css/Product.scss';
 import ProductBanner from './ProductBanner';
 
+export const displayProdAtom = atom(false);
+
 export default function ProductCard({company, name, rating, price, id}) {  
-  const [showProduct, setShowProduct] = useState(false);
+  const [showProduct, setShowProduct] = useAtom(displayProdAtom);
+  const [selectedProdId, setSelectedProdId] = useState(null);
+
+  function displayProduct(selectedId){
+    setSelectedProdId(selectedId);
+    setShowProduct(true);
+  }
 
   return (
     <section className='product'>
         <img src='#'/>
-        <a onClick={() => setShowProduct(true)}>{company}</a>
-        <a onClick={() => setShowProduct(true)}>{name}</a>
+        <a onClick={() => displayProduct(id)}>{company}</a>
+        <a onClick={() => displayProduct(id)}>{name}</a>
         <p>${price}</p>
         <div className='productBtns'>
-            <button>Add to Cart</button>
+            <button onClick={() => displayProduct(id)}>Add to Cart</button>
             <button>Favorite</button>
         </div>
-        {showProduct && <ProductBanner />}
+        {showProduct && selectedProdId === id && (
+        <ProductBanner 
+          productCompany={company} 
+          productName={name} 
+          productRating={rating} 
+          productPrice={price} 
+          productId={id}
+        />
+        )}
     </section>
+
   )
 }
